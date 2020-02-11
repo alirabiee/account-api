75246276613ee169fc938687c8b5ca5b5fed6b02
Ali Rabiee <alirabiee@users.noreply.github.com>
2020-02-11 22:39:29 +0100

    Add OpenAPI

--- src/main/java/ee/rabi/ali/api/account/Application.java
+++ src/main/java/ee/rabi/ali/api/account/Application.java
@@ -1,7 +1,18 @@
 package ee.rabi.ali.api.account;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts API",
+                version = "0.1",
+                description = "This API supports accounts with fiat currencies, hard over-draft limits, and real-time transfers",
+                contact = @Contact(url = "https://github.com/alirabiee", name = "Ali Rabiee")
+        )
+)
 public final class Application {
 
     public static void main(String[] args) {
