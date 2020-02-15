56c0064f086fcb3d6efd892e2310f7df8d68dada
Ali Rabiee <alirabiee@users.noreply.github.com>
2020-02-15 12:31:24 +0100

    Add test balance_shouldReturn404_givenAccountDoesNotExist

--- src/test/java/ee/rabi/ali/api/account/app/account/controller/AccountControllerTest.java
+++ src/test/java/ee/rabi/ali/api/account/app/account/controller/AccountControllerTest.java
@@ -7,6 +7,9 @@ import ee.rabi.ali.api.account.test.data.account.AccountTestData;
 import ee.rabi.ali.api.account.test.data.ledger.LedgerTestData;
 import io.micronaut.core.type.Argument;
 import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import io.micronaut.test.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +20,7 @@ import java.util.Currency;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @MicronautTest
 public class AccountControllerTest extends IntegrationTest {
@@ -43,13 +47,23 @@ public class AccountControllerTest extends IntegrationTest {
     void balance_shouldReturnCorrectBalance_givenData() {
         accountTestData.insertAccount1WithEUR();
         ledgerTestData.creditAccount1By1();
-        GetAccountBalanceResponse response = client
+        HttpResponse<GetAccountBalanceResponse> response = client
                 .toBlocking()
-                .retrieve(HttpRequest.GET("/account/1/balance"), GetAccountBalanceResponse.class);
+                .exchange(HttpRequest.GET("/account/1/balance"), GetAccountBalanceResponse.class);
+        assertEquals(HttpStatus.OK.getCode(), response.code());
         final GetAccountBalanceResponse expected = GetAccountBalanceResponse
                 .builder()
                 .balance(BigDecimal.ONE)
                 .build();
-        assertEquals(expected, response);
+        assertEquals(expected, response.body());
+    }
+
+    @Test
+    void balance_shouldReturn404_givenAccountDoesNotExist() {
+        final HttpClientResponseException exception = assertThrows(HttpClientResponseException.class, () -> client
+                .toBlocking()
+                .exchange(HttpRequest.GET("/account/1/balance")));
+        assertEquals(HttpStatus.NOT_FOUND, exception.getStatus());
+        assertEquals("Could not find account 1", exception.getMessage());
     }
 }
